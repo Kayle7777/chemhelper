cc1e96aec3b3dfb483c212404a111df693cd3872
Kayle7777 <jessewebb2@gmail.com>
2019-01-09 00:22:45 -0600

    Add recipes.json, as taken from Mysha's online recipes.yaml file, very nicely formatted. Thanks Mysha! These are the publicly available chemicals as listed on the SS13 wiki.

---
+++ src/pages/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({});
+
+const Main = props => {
+    return <div />;
+};
+
+export default withStyles(styles)(Main);
