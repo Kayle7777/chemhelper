4d6e9ccce52ae8b086c8fcf0dd91a5254f3ecbe7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-10 03:13:21 +0000

    Deploy Kayle7777/chemhelper to github.com/Kayle7777/chemhelper.git:gh-pages

--- precache-manifest.dfc3f759c2ee0f72a27d81b6b5a0f26f.js
+++ precache-manifest.dfc3f759c2ee0f72a27d81b6b5a0f26f.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/chemhelper/static/js/runtime~main.f0636d74.js"
   },
   {
-    "revision": "97c71b575725b1a86ff6",
-    "url": "/chemhelper/static/js/main.97c71b57.chunk.js"
+    "revision": "bba1a5e03bb29d19d7dd",
+    "url": "/chemhelper/static/js/main.bba1a5e0.chunk.js"
   },
   {
     "revision": "6f245d0001cc27323f9b",
     "url": "/chemhelper/static/js/1.6f245d00.chunk.js"
   },
   {
-    "revision": "a3ea4992b32b10abd9aaf5929183452e",
+    "revision": "101958d92a21094f8ff268b9dda02af8",
     "url": "/chemhelper/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chemhelper/precache-manifest.3adfc5b07777cb1e061fba4821c750c6.js"
+  "/chemhelper/precache-manifest.dfc3f759c2ee0f72a27d81b6b5a0f26f.js"
 );
 
 workbox.clientsClaim();
