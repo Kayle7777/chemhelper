e8acd9c917d527795cb7bb74cfdbecbb79eefb5a
Kayle7777 <jessewebb2@gmail.com>
2019-01-21 09:36:23 -0600

    woops

--- src/components/ChemTable.js
+++ src/components/ChemTable.js
@@ -27,7 +27,7 @@ const styles = theme => ({
 });
 
 const ChemTable = props => {
-    const { classes, recipes } = props;
+    const { recipes } = props;
     return (
         <AutoSizer>
             {({ width, height }) => (
