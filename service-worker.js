76bc88351cd2279d75a417f4e0308df148490141
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-11 13:54:08 +0000

    Deploy Kayle7777/chemhelper to github.com/Kayle7777/chemhelper.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chemhelper/precache-manifest.46cdd047e34bc9e1ae105f4838c1af27.js"
+  "/chemhelper/precache-manifest.5937c33c202c2bf1f82d1c4d0081c137.js"
 );
 
 workbox.clientsClaim();
